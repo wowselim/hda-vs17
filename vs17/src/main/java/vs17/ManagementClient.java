9899a206f39ff2011e2fb4f9afb31746203ab274
Selim Dincer <wowselim@live.de>
2017-05-01 10:41:03 +0200

    simplify code

--- vs17/src/main/java/vs17/ManagementClient.java
+++ vs17/src/main/java/vs17/ManagementClient.java
@@ -8,25 +8,15 @@ public class ManagementClient {
 	public static void main(String[] args) {
 		String host = "";
 		int port = -1;
-		if (args.length != 2) {
-			throw new RuntimeException("Usage: <remoteHost> <remotePort>");
-		}
-		for (int i = 0; i < args.length; i++) {
-			try {
-				if (i == 0) {
-					host = args[0];
-				} else if (i == 1) {
-					port = Integer.parseInt(args[1]);
-				}
-			} catch (Exception e) {
-				throw new RuntimeException("Usage: <remoteHost> <remotePort>", e);
-			}
+		try {
+			host = args[0];
+			port = Integer.parseInt(args[1]);
+		} catch (Exception e) {
+			throw new RuntimeException("Usage: <remoteHost> <remotePort>", e);
 		}
 
-		DatagramSocket clientSocket = null;
-		try {
+		try (DatagramSocket clientSocket = new DatagramSocket()) {
 			System.out.printf("Sending ping packet to %s on port %d.%n", host, port);
-			clientSocket = new DatagramSocket();
 			DatagramPacket pingPacket = new DatagramPacket(new byte[1], 1, InetAddress.getByName("selim.co"), 1338);
 			clientSocket.send(pingPacket);
 			byte[] incomingData = new byte[512];
@@ -36,10 +26,6 @@ public class ManagementClient {
 			System.out.println(history);
 		} catch (Exception e) {
 			e.printStackTrace();
-		} finally {
-			if (clientSocket != null) {
-				clientSocket.close();
-			}
 		}
 	}
 }
